c2d064333eb12b013a6c3888afecf94d0f9f0e21
huzy <359216047@qq.com>
2019-07-31 13:55:06 +0800

    h

--- main.sh
+++ main.sh
@@ -38,10 +38,10 @@ IO_size_array=("128M" "512M" "1G" "2G" \
 				"1G" "1G")
 for test_id in {1..22}
 do
-	batch = Block_size_array[test_id]
-	io_t = IO_type_array[test_id]
-	io_s = IO_size_array[test_id]
-	result = "test_${test_id}"
+	batch=Block_size_array[test_id]
+	io_t=IO_type_array[test_id]
+	io_s=IO_size_array[test_id]
+	result="test_${test_id}"
 	echo "================== ${result} begins, i/o type ${batch}; batchsize ${batch}; i/o size ${io_s};  ===================" | tee -a ~/results.txt
 	fio -direct=1 -iodepth 1 -thread  -ioengine=psync -rw=${io_t} -bs=${batch} -io_size=${io_s} -size=512M -numjobs=10 -runtime=60 -group_reporting  -fallocate=none -name=${result} -directory=/mnt/orangefs | tee -a ~/${result}.txt
 done
